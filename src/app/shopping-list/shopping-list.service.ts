284a33fd3cd1f952ec1c17d8edcf573df058c562
Bhavesh <bhaveshjagtap21@gmail.com>
2021-12-06 20:26:00 +0530

    To make the delete button work.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -37,4 +37,9 @@ export class ShoppingListService {
         this.ingredientsChanged.next(this.ingredients.slice());
         // call this item on onadditem s-e-c.ts
     }
+
+    deleteIngredient(index: number){
+        this.ingredients.splice(index, 1);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
