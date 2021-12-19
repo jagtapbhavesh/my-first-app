d0759a7c585286f70f966fddbd8c0050aa38f74e
Bhavesh <bhaveshjagtap21@gmail.com>
2021-12-19 18:11:27 +0530

    adding a loading spinner and error handling logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -6,5 +9,39 @@ import { Component } from "@angular/core";
 
 })
 export class AuthComponent {
-    
+    isLoginMode = true;
+    isLoading = false;
+    error: string = null;
+
+    constructor(private authservice: AuthService) {}
+
+    onSwitchMode(){
+        this.isLoginMode = !this.isLoginMode;
+    }
+
+    onSubmit(form: NgForm){
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        this.isLoading = true;
+        if (this.isLoginMode) {
+            // ....
+        } else {
+            this.authservice.signup(email,password).subscribe(
+                resData => {
+                    console.log(resData);
+                    this.isLoading = false;
+                },
+                errorMessage => {
+                    console.log(errorMessage);
+                    this.error = errorMessage;
+                    this.isLoading = false;
+                }
+            );
+        }
+        form.reset();
+    }
 }
